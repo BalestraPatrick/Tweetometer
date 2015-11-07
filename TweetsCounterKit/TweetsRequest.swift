bfcfdbeaee2fdbe092c2d35fe444fb8c0bc2020f
Patrick Balestra <me@patrickbalestra.com>
2015-11-07 12:42:28 +0100

    Removed unused Pods and Frameworks

--- TweetsCounterKit/TweetsRequest.swift
+++ TweetsCounterKit/TweetsRequest.swift
@@ -7,14 +7,20 @@
 //
 
 import Foundation
+import RxAlamofire
 import Alamofire
 import RxSwift
 
-public struct TweetsRequest {
+public class TweetsRequest {
 
     public init() {
         let url = NSURL(string: TwitterAPI().userTimelineURL)!
+        let disposeBag = DisposeBag()
         
+        requestJSON(Method.GET, url)
+            .observeOn(MainScheduler.sharedInstance)
+            .debug()
+            .subscribe{ print($0) }
 //        let observable = Alamofire.request(Method.GET, url).rx_response()
 //        observable.observeOn(MainScheduler.sharedInstance)
 //            .subscribe(onNext: { json in
