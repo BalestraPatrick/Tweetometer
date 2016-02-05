40dff1977a79c29f29c43753ce0181c0917d7fc8
Patrick Balestra <me@patrickbalestra.com>
2016-02-05 16:01:16 +0100

    Refactoring unit tests

---
+++ TweetsCounterTests/MenuOptionsTests.swift
@@ -0,0 +1,25 @@
+//
+//  MenuOptionsTests.swift
+//  TweetsCounter
+//
+//  Created by Patrick Balestra on 2/5/16.
+//  Copyright © 2016 Patrick Balestra. All rights reserved.
+//
+
+import XCTest
+@testable import TweetsCounter
+
+class MenuOptionsTests: XCTestCase {
+    
+    func testDataSource() {
+        let options = MenuOptionsDataSource().options
+        
+        XCTAssertEqual(options.count, 3)
+        XCTAssertEqual(options[0].title, "Refresh")
+        XCTAssertEqual(options[0].image, "Refresh")
+        XCTAssertEqual(options[1].title, "Logout")
+        XCTAssertEqual(options[1].image, "Logout")
+        XCTAssertEqual(options[2].title, "About")
+        XCTAssertEqual(options[2].image, "About")
+    }
+}
